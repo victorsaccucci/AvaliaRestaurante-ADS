f867df5ed058ac38755f9dd0c8e9923757fe632c
Victor Saccucci <victorsaccucciv@gmail.com>
2023-11-07 18:13:35 -0300

    Initial commit

---
+++ src/main/java/com/SenacQuartaFase/AvaliaRestaurante/AvaliaRestauranteApplication.java
@@ -0,0 +1,13 @@
+package com.SenacQuartaFase.AvaliaRestaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliaRestauranteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliaRestauranteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SenacQuartaFase/AvaliaRestaurante/AvaliaRestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SenacQuartaFase.AvaliaRestaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliaRestauranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
