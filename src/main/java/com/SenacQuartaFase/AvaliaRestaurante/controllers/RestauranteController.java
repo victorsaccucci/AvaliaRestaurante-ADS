09512467fef400a8c84f8a7e8a68cf6c502172da
Victor Saccucci <victorsaccucciv@gmail.com>
2023-11-10 09:39:41 -0300

    Add CrossOrigin

--- src/main/java/com/SenacQuartaFase/AvaliaRestaurante/controllers/RestauranteController.java
+++ src/main/java/com/SenacQuartaFase/AvaliaRestaurante/controllers/RestauranteController.java
@@ -7,9 +7,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:4200","http://localhost:5500"}, maxAge = 3600)
 @RequestMapping(value = "/api/restaurante")
 public class RestauranteController {
 
