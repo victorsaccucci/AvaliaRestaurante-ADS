556b84eff26085fa6285a6f1dd2fcee88e771797
ArthurSauerr <germanooarthuur@gmail.com>
2023-11-15 12:58:42 -0300

    Implementado métodos para validação e finalização do CRUD

--- src/main/java/com/SenacQuartaFase/AvaliaRestaurante/services/PessoaService.java
+++ src/main/java/com/SenacQuartaFase/AvaliaRestaurante/services/PessoaService.java
@@ -1,6 +1,8 @@
 package com.SenacQuartaFase.AvaliaRestaurante.services;
 
+import com.SenacQuartaFase.AvaliaRestaurante.entities.Endereco;
 import com.SenacQuartaFase.AvaliaRestaurante.entities.Pessoa;
+import com.SenacQuartaFase.AvaliaRestaurante.exceptions.AvaliaRestauranteException;
 import com.SenacQuartaFase.AvaliaRestaurante.repositories.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,11 +13,49 @@ public class PessoaService {
     @Autowired
     private PessoaRepository repository;
 
-    public Pessoa salvar(Pessoa novoVisitante){
+    public Pessoa salvar(Pessoa novoVisitante) throws AvaliaRestauranteException{
+        validarCamposObrigatorios(novoVisitante);
         return repository.save(novoVisitante);
     }
 
     public Pessoa buscarId(Long id){
         return repository.buscarPeloId(id);
+    }
+
+    public Pessoa atualizar(Pessoa visitante) throws AvaliaRestauranteException{
+        validarCamposObrigatorios(visitante);
+        return repository.save(visitante);
+    }
+
+    public boolean excluir(Long id){
+        repository.deleteById(id.longValue());
+        return true;
+    }
+
+    private void validarCamposObrigatorios(Pessoa pessoa) throws AvaliaRestauranteException{
+        String mensagemValidacao = "";
+        mensagemValidacao += validarCampoString(pessoa.getNome(), "nome");
+        mensagemValidacao += validarCampoString(pessoa.getCpf(), "cpf");
+        mensagemValidacao += validarCampoString(pessoa.getEmail(), "email");
+        mensagemValidacao += validarCampoString(pessoa.getSenha(), "senha");
+        mensagemValidacao += validarCampoEndereco(pessoa.getEndereco(), "endereco");
+
+        if(mensagemValidacao.isEmpty()){
+            throw new AvaliaRestauranteException(mensagemValidacao);
+        }
+    }
+
+    private String validarCampoString(String valorCampo, String nomeCampo) {
+        if(valorCampo == null || valorCampo.trim().isEmpty()){
+            return "Informe o " +nomeCampo + "\n";
+        }
+        return "";
+    }
+
+    private String validarCampoEndereco(Endereco valorCampo, String nomeCampo) {
+        if(valorCampo == null){
+            return "Informe o " +nomeCampo + "\n";
+        }
+        return "";
     }
 }
