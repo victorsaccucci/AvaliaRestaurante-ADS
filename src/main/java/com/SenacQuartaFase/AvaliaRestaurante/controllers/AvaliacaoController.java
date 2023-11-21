637200cf7b78516b706dff1bbead58859b0ab6cd
Arthur Sauer <germanooarthuur@gmail.com>
2023-11-21 10:31:51 -0300

    Implementado validação para excluir avaliação

--- src/main/java/com/SenacQuartaFase/AvaliaRestaurante/controllers/AvaliacaoController.java
+++ src/main/java/com/SenacQuartaFase/AvaliaRestaurante/controllers/AvaliacaoController.java
@@ -1,6 +1,7 @@
 package com.SenacQuartaFase.AvaliaRestaurante.controllers;
 
 import com.SenacQuartaFase.AvaliaRestaurante.entities.Avaliacao;
+import com.SenacQuartaFase.AvaliaRestaurante.exceptions.AvaliaRestauranteException;
 import com.SenacQuartaFase.AvaliaRestaurante.services.AvaliacaoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -16,5 +17,10 @@ public class AvaliacaoController {
     @PostMapping
     public Avaliacao salvar(@RequestBody Avaliacao avaliacao){
         return service.salvar(avaliacao);
+    }
+
+    @PostMapping("/excluir")
+    public void excluir(@RequestBody Avaliacao avaliacao) throws AvaliaRestauranteException {
+        service.deletarAvaliacao(avaliacao);
     }
 }
--- src/main/java/com/SenacQuartaFase/AvaliaRestaurante/services/AvaliacaoService.java
+++ src/main/java/com/SenacQuartaFase/AvaliaRestaurante/services/AvaliacaoService.java
@@ -1,6 +1,8 @@
 package com.SenacQuartaFase.AvaliaRestaurante.services;
 
 import com.SenacQuartaFase.AvaliaRestaurante.entities.Avaliacao;
+import com.SenacQuartaFase.AvaliaRestaurante.entities.Pessoa;
+import com.SenacQuartaFase.AvaliaRestaurante.exceptions.AvaliaRestauranteException;
 import com.SenacQuartaFase.AvaliaRestaurante.repositories.AvaliacaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,4 +16,15 @@ public class AvaliacaoService {
     public Avaliacao salvar(Avaliacao avaliacao){
         return repository.save(avaliacao);
     }
+
+    public void deletarAvaliacao(Avaliacao avaliacaoParaExcluir) throws AvaliaRestauranteException {
+        Pessoa quemQuerExcluirAvaliacao = avaliacaoParaExcluir.getPessoa();
+        Avaliacao avaliacaoNoBanco = this.repository.getReferenceById(avaliacaoParaExcluir.getId());
+
+        if (quemQuerExcluirAvaliacao != avaliacaoNoBanco.getPessoa()) {
+            throw new AvaliaRestauranteException("Usuário sem permissâo para excluir");
+        }
+        repository.delete(avaliacaoParaExcluir);
+    }
+
 }
